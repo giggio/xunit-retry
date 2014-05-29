e46641579f62320129cb1469c9ce206751297f16
Giovanni Bassi <giggio@giggio.net>
2014-05-29 19:31:26 -0300

    renomeando arquivos e projetos

--- Test/SpecFlowFeature1.feature.cs
+++ Test/SpecFlowFeature1.feature.cs
@@ -11,7 +11,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace XUnitTestProject1
+namespace Test
 {
     using TechTalk.SpecFlow;
     
