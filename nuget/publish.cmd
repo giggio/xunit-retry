1e146199f600a62ec09fa9939e222e182b643e29
Giovanni Bassi <giggio@giggio.net>
2014-05-30 15:03:41 -0300

    fix publish script

--- nuget/publish.cmd
+++ nuget/publish.cmd
@@ -1,3 +1,3 @@
 call build.cmd
-nuget push "%~dp0XunitRetry.1*.nupkg" %*
-nuget push "%~dp0XunitRetry.Generator*.nupkg" %*
+call nuget push "%~dp0XunitRetry.1*.nupkg" %*
+call nuget push "%~dp0XunitRetry.Generator*.nupkg" %*
