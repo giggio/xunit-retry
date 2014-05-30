cb06be6e96e9137b2b40a19e4f5482cecb4fdd2b
Giovanni Bassi <giggio@giggio.net>
2014-05-30 13:59:55 -0300

    set assemblyinfo info and add nuget folder to sln

--- XUnitRetry/Properties/AssemblyInfo.cs
+++ XUnitRetry/Properties/AssemblyInfo.cs
@@ -2,35 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("XUnitRetry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("XunitRetry")]
+[assembly: AssemblyDescription("A Xunit plugin that allows a test to try again if it fails.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XUnitRetry")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Lambda3")]
+[assembly: AssemblyProduct("XunitRetry")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("43c8b633-f8ae-4ca3-84e0-f2110b5fb488")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- XunitRetry.Generator.SpecflowPlugin/Properties/AssemblyInfo.cs
+++ XunitRetry.Generator.SpecflowPlugin/Properties/AssemblyInfo.cs
@@ -2,35 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CustomXUnit.Generator.SpecflowPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("XunitRetry.Generator.SpecflowPlugin")]
+[assembly: AssemblyDescription("A SpecFlow plugin that allows a spec to retry a number of times if it fails.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomXUnit.Generator.SpecflowPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Lambda3")]
+[assembly: AssemblyProduct("XunitRetry")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("69ea94cc-779b-4a9e-95c6-8f49ed6539fa")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
