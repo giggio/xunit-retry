0845feca076edd3eae1a44072b1a5a61491a458e
Kim Birkelund <kim@birkelund.me>
2019-09-03 13:01:10 +0200

    Convert to .NET Standard.

--- XUnitRetry/Properties/AssemblyInfo.cs
+++ XUnitRetry/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("XunitRetry")]
-[assembly: AssemblyDescription("A Xunit plugin that allows a test to try again if it fails.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Lambda3")]
-[assembly: AssemblyProduct("XunitRetry")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("43c8b633-f8ae-4ca3-84e0-f2110b5fb488")]
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
