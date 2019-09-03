d79bc76c3b5899134810c7ff3a50c29cf3ad7795
Kim Birkelund <kim@birkelund.me>
2019-09-03 13:37:03 +0200

    Added support for retrying theories.

---
+++ XUnitRetry/RetryTheoryAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Xunit.Sdk;
+
+namespace Xunit {
+    /// <summary>
+    /// Works just like [Fact] except that failures are retried (by default, 3 times).
+    /// </summary>
+    [XunitTestCaseDiscoverer("Xunit.RetryTheoryDiscoverer", "XunitRetry")]
+    public class RetryTheoryAttribute : TheoryAttribute
+    {
+        public RetryTheoryAttribute(int maxRetries = 3)
+        {
+            MaxRetries = maxRetries;
+        }
+
+        /// <summary>
+        /// Number of retries allowed for a failed test. If unset (or set less than 1), will
+        /// default to 3 attempts.
+        /// </summary>
+        public int MaxRetries { get; set; }
+    }
+}
